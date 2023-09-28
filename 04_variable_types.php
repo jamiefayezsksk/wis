182d1eb2f8ad8dbe6b88cd87587928e8a701662c
jamiefayezsksk <jimejamie76@gmail.com>
2023-09-29 07:46:00 +0800

    test push

--- 04_variable_types.php
+++ 04_variable_types.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=S, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $int_var = 12345;
-        $another_int = -12345 + 12345;
+    $int_var = 12345;
+    $another_int = -12345 + 12345;
+    print $int_var;
     ?>
 </body>
+
 </html>
