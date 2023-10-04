96f9d77eb5fc5a39c7996b750b0955e222a3a644
jamiefayezsksk <jimejamie76@gmail.com>
2023-10-04 18:22:00 +0800

    operator

--- 06_operator_tyes.php
+++ 06_operator_tyes.php
@@ -12,20 +12,22 @@
     $a = 42;
     $b = 20;
 
+    echo "Operator Types<br/><br/>";
+
     $c = $a + $b;
-    echo "Addition Operation Result: $c <br/>";
+    echo "Addition Operation Result: $c <br/><br/>";
     $c = $a - $b;
-    echo "Subtraction Operation Result: $c <br/>";
+    echo "Subtraction Operation Result: $c <br/><br/>";
     $c = $a * $b;
-    echo "Multiplication Operation Result: $c <br/>";
+    echo "Multiplication Operation Result: $c <br/><br/>";
     $c = $a / $b;
-    echo "Division Operation Result: $c <br/>";
+    echo "Division Operation Result: $c <br/><br/>";
     $c = $a % $b;
-    echo "Modulus Operation Result: $c <br/>";
+    echo "Modulus Operation Result: $c <br/><br/>";
     $c = $a++;
-    echo "Increment Operation Result: $c <br/>";
+    echo "Increment Operation Result: $c <br/><br/>";
     $c = $a--;
-    echo "Decrement Operation Result: $c <br/>";
+    echo "Decrement Operation Result: $c <br/><br/>";
     ?>
 </body>
 
