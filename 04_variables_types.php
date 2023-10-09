f3d7a83b52bac031c32bbf1bddf25c8cd03c0833
jamiefayezsksk <jimejamie76@gmail.com>
2023-10-09 17:14:14 +0800

    syntax overview

---
+++ 04_variables_types.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=S, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $int_var = 12345;
+        $another_int = -12345 + 12345;
+        $many = 2.2888800;
+        $many_2 = 2.2111200;
+        $few = $many + $many_2;
+        print(.$many + $many_2 = $few<br>.);
+    ?>
+</body>
+</html>
