23e3bf9963858ac0eb03cea46df963e895775de6
jamiefayezsksk <@jimejamie76@gmail.com>
2023-12-04 19:39:48 +0800

    updated with updte and delete options

--- courses.php
+++ courses.php
@@ -7,83 +7,180 @@
     <title>CourseRecord</title>
 </head>
 <body>
-    <?php // Check if the query was successful
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "studentrecord";
+    <ul class="navigation-bar">
+        <li><a href="Student_record.php">StudentRecord</a></li>
+        <li><a href="Course_Record.php">Course</a></li>
+        <li><a href="Instructor_Record.php">Instructor</a></li>
+        <li><a href="Enrollment_Record.php">Enrollment</a></li>
+    </ul>
+    <div class="status">
+        <?php // Check if the query was successful
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "studentrecord";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Status:  Connection failed: " . $conn->connect_error);
-    }
-    echo "Server Status: Connected successfully";
-    ?>
-    <hr>
-    <h1>Add Course</h1>
-    <table style="width:40%">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            <tr><td><label for="fname">Course Name:</label></td>
-            <td><input type="text" name="coursename" id="coursename" value=""></td></tr>
-            <tr><td><label for="fname">Credits:</label></td> 
-            <td><input type="text" name="coursecredits" id="coursecredits" value=""></td></tr>
-            <tr><td></td><td><input type="submit" value="Add Course" name="submit"></td></tr>
-        </form>
-    </table>
-
-
-    <h1>Course Records</h1>
-    <?php 
-
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        try{
-            $coursename = $_POST['coursename'];
-            $coursecredit = (int)$_POST['coursecredits'];
-            $studentsql = "INSERT INTO course (CourseName,Credits) 
-                                    VALUES(
-                                    '$coursename',
-                                    '$coursecredit')";
-            //$studentrecord = $conn->exec($studentsql);
-            //echo  gettype($studenfname);	
-
-            if (mysqli_query($conn, $studentsql)) {
-                echo "New record created successfully";
-            } else {
-                echo "<br>Error: " . $studentsql . "<br>" . mysqli_error($conn);
-            }
-        }catch(PDOException $e) {
-            echo $studentrecord . "<br>" . $e->getMessage();
+        // Check connection
+        if ($conn->connect_error) {
+            die("Status:  Connection failed: " . $conn->connect_error);
         }
-        
-        
-    }?>
-    <table style="width:100%">
-        <tr>
-            <th>CourseID</th>
-            <th>CourseName</th>
-            <th>Credits</th>
-        </tr>
-        <?php
-        echo "<br><hr>";
-        // Example query
-        $sql = "SELECT * FROM course";
-        $result = $conn->query($sql);
+        echo "Server Status: Connected successfully";
+        ?>
+    </div>
 
-        // Check if the query was successful
-        if ($result) {
-            // Process the results
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr><td>" . $row["CourseID"] . "</td>"
-                . "<td>" . $row["CourseName"]. "</td>"
-                . "<td>" . $row["Credits"]. "</td></tr>";
+    <div class="card-style">
+        <h1>Add Course</h1>
+        <table style="width:40%">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <tr><td><label for="fname">Course Name:</label></td>
+                <td><input type="text" name="coursename" id="coursename" value=""></td></tr>
+                <tr><td><label for="fname">Credits:</label></td> 
+                <td><input type="text" name="coursecredits" id="coursecredits" value=""></td></tr>
+                <tr><td></td><td><input type="submit" value="Add Course" name="addcourse"></td></tr>
+            </form>
+        </table>
+    </div>
+
+
+    <div class="card-style">
+        <h1>Course Records</h1>
+        <?php 
+
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['addcourse']))
+        {
+            try{
+                $coursename = $_POST['coursename'];
+                $coursecredit = (int)$_POST['coursecredits'];
+                $coursesql = "INSERT INTO course (CourseName,Credits) 
+                                        VALUES(
+                                        '$coursename',
+                                        '$coursecredit')";
+                //$studentrecord = $conn->exec($studentsql);
+                //echo  gettype($studenfname);	
+
+                if (mysqli_query($conn, $coursesql)) {
+                    echo "New record created successfully";
+                } else {
+                    echo "<br>Error: " . $studentsql . "<br>" . mysqli_error($conn);
+                }
+            }catch(PDOException $e) {
+                echo $studentrecord . "<br>" . $e->getMessage();
             }
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            
+            
         }?>
-        </table><hr>
+            <table style="width:100%">
+                <tr>
+                    <th>CourseID</th>
+                    <th>CourseName</th>
+                    <th>Credits</th>
+                    <th>Options</th>
+                </tr>
+                <?php
+                // Example query
+                $sql = "SELECT * FROM course";
+                $result = $conn->query($sql);
+
+                // Check if the query was successful
+                if ($result) {
+                    // Process the results
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr><td>" . $row["CourseID"] . "</td>"
+                        . "<td>" . $row["CourseName"]. "</td>"
+                        . "<td>" . $row["Credits"]. "</td>"
+                        . "<td><form method=".'POST'.">" 
+                        . "<input type=".'hidden'." value=". '_method' ." name= " . "DELETE"  ."/>" 
+                        . "<button type=".'submit'." value=". $row["CourseID"] ." name= " . 'deleteButton' .">Delete</button>" 
+                        . "</form></td></tr>";
+                    }
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }?>
+            </table>
+        
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteButton'])) {
+                        $idToDelete = $_POST['deleteButton'];;
+
+                        $sql = "DELETE FROM course WHERE CourseID=$idToDelete";
+
+                        if ($conn->query($sql) === TRUE) {
+                            echo "Record deleted successfully";
+                        } else {
+                            echo "Error deleting record: " . $conn->error;
+                        }
+                        // echo "Delete operation triggered.";
+                }
+            ?>
+        </div>
+
+        <div class="card-style">
+        <?php 
+        $selecteditsql = "SELECT CourseID, CourseName, Credits  FROM course";
+        $result = $conn->query($selecteditsql);
+        ?>
+        <h1>Edit Records</h1>
+        <form method="POST">
+            <label for="course_id">Select Course</label>
+            <select name="course_id" id="course_id">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<option value="' . $row['CourseID'] . '">' . $row['CourseName'] ." "." (". $row['Credits'].") " . '</option>';
+                    }
+                }
+                ?>
+            </select>
+            <input type="submit" value="Show Student Info">
+        </form>
+        <?php
+    // Check if the form has been submitted and a student ID is selected
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['course_id'])) {
+        $selected_id = $_POST['course_id'];
+
+        // Fetch student details based on the selected ID
+        $editsql = "SELECT * FROM course WHERE CourseID = $selected_id";
+        $result = $conn->query($editsql);
+
+        if ($result->num_rows > 0) {
+            $editstudent = $result->fetch_assoc();
+            // Display input fields with fetched student information
+            ?>
+            <table style="width:40%">
+            <form method="POST" action="Course_Record.php">
+            <input type="hidden" name="ecourse_id" value="<?php echo $editstudent['CourseID']; ?>">
+            <tr><td>Course Name:</td>
+             <td><input type="text" name="efirstname" value="<?php echo $editstudent['CourseName']; ?>"></td></tr>
+             <tr><td>Last Name:</td>
+             <td><input type="text" name="elastname" value="<?php echo $editstudent['Credits']; ?>"></td></tr>
+             <tr><td></td><td><input type="submit" value="Update"></td></tr>
+            </form>
+            </table>
+            <?php
+        } else {
+            echo "No Course found with this ID.";
+        }
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ecourse_id'])) {
+        $ucourse_id = $_POST['ecourse_id'];
+        $ucoursename = $_POST['efirstname'];
+        $ucredits = $_POST['elastname'];
+
+    
+        $eupdatesql = "UPDATE course SET CourseName='$ucoursename', Credits='$ucredits' WHERE CourseID='$ucourse_id'";
+    
+        if ($conn->query($eupdatesql) === TRUE) {
+            echo "Course information updated successfully";
+        } else {
+            echo "Error updating Course information: " . $conn->error;
+        }
+    }
+
+    ?>
+    </div>
 </body>
 </html>
--- instructor.php
+++ instructor.php
@@ -7,46 +7,50 @@
     <title>InstructorRecord</title>
 </head>
 <body>
-    <?php // Check if the query was successful
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "studentrecord";
+    <div class="status">
+        <?php // Check if the query was successful
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "studentrecord";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Status:  Connection failed: " . $conn->connect_error);
-    }
-    echo "Server Status: Connected successfully";
-    ?>
-    <hr>
-    <h1>Add Instructor Record</h1>
-    <table style="width:40%">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            <tr><td><label for="fname">First name:</label></td>
-            <td><input type="text" name="insfname" id="insfname" value=""></td></tr>
-            <tr><td><label for="fname">Last name:</label></td>
-            <td><input type="text" name="inslname" id="inslname" value=""></td></tr>
-            <tr><td><label for="fname">Email:</label></td>
-            <td><input type="text" name="insemail" id="insemail" value=""></td></tr>
-            <tr><td><label for="fname">Phone:</label></td>
-            <td><input type="text" name="insphone" id="insphone" value=""></td></tr> 
-            <tr><td></td><td><input type="submit" value="submit" name="submit"></td></tr>
-        </form>
-    </table>
+        // Check connection
+        if ($conn->connect_error) {
+            die("Status:  Connection failed: " . $conn->connect_error);
+        }
+        echo "Server Status: Connected successfully";
+        ?>
+    </div>
+
+    <div class="card-style">
+        <h1>Add Instructor Record</h1>
+        <table style="width:40%">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <tr><td><label for="fname">First name:</label></td>
+                <td><input type="text" name="insfname" id="insfname" value=""></td></tr>
+                <tr><td><label for="fname">Last name:</label></td>
+                <td><input type="text" name="inslname" id="inslname" value=""></td></tr>
+                <tr><td><label for="fname">Email:</label></td>
+                <td><input type="text" name="insemail" id="insemail" value=""></td></tr>
+                <tr><td><label for="fname">Phone:</label></td>
+                <td><input type="text" name="insphone" id="insphone" value=""></td></tr> 
+                <tr><td></td><td><input type="submit" value="submit" name="addinstructor"></td></tr>
+            </form>
+        </table>
+    </div>
     <?php 
 
-    if($_SERVER["REQUEST_METHOD"] == "POST")
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['addinstructor']))
     {
         try{
             $studenfname = $_POST['insfname'];
             $studentlname = $_POST['inslname'];
             $studentemail = $_POST['insemail'];
             $studentphone = (int)$_POST['insphone'];
-            $studentsql = "INSERT INTO instructor (FirstName,LastName,Email,Phone) 
+            $instrucsql = "INSERT INTO instructor (FirstName,LastName,Email,Phone) 
                                     VALUES(
                                     '$studenfname',
                                     '$studentlname',
@@ -55,10 +59,10 @@
             //$studentrecord = $conn->exec($studentsql);
             //echo  gettype($studenfname);	
 
-            if (mysqli_query($conn, $studentsql)) {
+            if (mysqli_query($conn, $instrucsql)) {
                 echo "New record created successfully";
             } else {
-                echo "<br>Error: " . $studentsql . "<br>" . mysqli_error($conn);
+                echo "<br>Error: " . $instrucsql . "<br>" . mysqli_error($conn);
             }
         }catch(PDOException $e) {
             echo $studentrecord . "<br>" . $e->getMessage();
@@ -66,35 +70,124 @@
         
         
     }?>
-    <h1>Instructor Records</h1>
-    <table style="width:100%">
-    <tr>
-        <th>Instructor ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-    </tr>
-    <?php
-    echo "<br><hr>";
-    // Example query
-    $sql = "SELECT * FROM instructor";
-    $result = $conn->query($sql);
 
-    // Check if the query was successful
-    if ($result) {
-        // Process the results
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr><td>" . $row["InstructorID"] . "</td>"
-            . "<td>" . $row["FirstName"]. "</td>"
-            . "<td>" . $row["LastName"]. "</td>"
-            . "<td>" . $row["Email"]. "</td>"
-            . "<td>" . $row["Phone"]. "</td></tr>";
+    <div class="card-style">
+        <h1>Instructor Records</h1>
+        <table style="width:100%">
+        <tr>
+            <th>Instructor ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Options</th>
+        </tr>
+        <?php
+        $sql = "SELECT * FROM instructor";
+        $result = $conn->query($sql);
+
+        // Check if the query was successful
+        if ($result) {
+            // Process the results
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["InstructorID"] . "</td>"
+                . "<td>" . $row["FirstName"]. "</td>"
+                . "<td>" . $row["LastName"]. "</td>"
+                . "<td>" . $row["Email"]. "</td>"
+                . "<td>" . $row["Phone"]. "</td>"
+                . "<td><form method=".'POST'.">" 
+                . "<input type=".'hidden'." value=". '_method' ." name= " . "DELETE"  ."/>" 
+                . "<button type=".'submit'." value=". $row["InstructorID"] ." name= " . 'deleteButton' .">Delete</button>" 
+                . "</form></td></tr>";
+            }
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }?>
+        </table>
+    </div>
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteButton'])) {
+                $idToDelete = $_POST['deleteButton'];;
+
+                $delinssql = "DELETE FROM instructor WHERE InstructorID=$idToDelete";
+
+                if ($conn->query($delinssql) === TRUE) {
+                    echo "Record deleted successfully";
+                } else {
+                    echo "Error deleting record: " . $conn->error;
+                }
         }
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }?>
-    </table><hr>
+    ?>
+    <div class="card-style">
+        <?php 
+            $selecteditsql = "SELECT InstructorID, FirstName, LastName, Email  FROM instructor";
+            $result = $conn->query($selecteditsql);
+            ?>
+            <h1>Edit Records</h1>
+            <form method="POST">
+                <label for="instr_id">Select Instructor</label>
+                <select name="instr_id" id="instr_id">
+                    <?php
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<option value="' . $row['InstructorID'] . '">' . $row['FirstName'] ." ". $row['LastName']."  (". $row['Email'].") " . '</option>';
+                        }
+                    }
+                    ?>
+                </select>
+                <input type="submit" value="Show Instructor Info">
+            </form>
+            <?php
+        // Check if the form has been submitted and a student ID is selected
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['instr_id'])) {
+            $selected_id = $_POST['instr_id'];
+
+            // Fetch student details based on the selected ID
+            $editsql = "SELECT * FROM instructor WHERE InstructorID = $selected_id";
+            $result = $conn->query($editsql);
+
+            if ($result->num_rows > 0) {
+                $editstudent = $result->fetch_assoc();
+                // Display input fields with fetched student information
+                ?>
+                <table style="width:40%">
+                <form method="POST" action="Instructor_Record.php">
+                <input type="hidden" name="instrs_id" value="<?php echo $editstudent['InstructorID']; ?>">
+                <tr><td>First Name:</td>
+                <td><input type="text" name="ifirstname" value="<?php echo $editstudent['FirstName']; ?>"></td></tr>
+                <tr><td>Last Name:</td>
+                <td><input type="text" name="ilastname" value="<?php echo $editstudent['LastName']; ?>"></td></tr>
+                <tr><td>Email:</td>
+                <td><input type="text" name="iemail" value="<?php echo $editstudent['Email']; ?>"></td></tr>
+                <tr><td>Phone: </td>
+                <td><input type="text" name="iphone" value="<?php echo $editstudent['Phone']; ?>"></td></tr>
+                <tr><td></td><td><input type="submit" value="Update"></td></tr>
+                </form>
+                </table>
+                <?php
+            } else {
+                echo "No student found with this ID.";
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['instrs_id'])) {
+            $instr_ids = $_POST['instrs_id'];
+            $ifirstnames = $_POST['ifirstname'];
+            $ilastnames = $_POST['ilastname'];
+            $iemails = $_POST['iemail'];
+            $iphones = $_POST['iphone'];
+        
+            $eupdatesql = "UPDATE instructor SET FirstName='$ifirstnames', LastName='$ilastnames', Email='$iemails', Phone='$iphones' WHERE InstructorID='$instr_ids'";
+        
+            if ($conn->query($eupdatesql) === TRUE) {
+                echo "Instructor information updated successfully";
+            } else {
+                echo "Error updating Instructor information: " . $conn->error;
+            }
+        }
+
+        ?>
+    </div>
 </body>
 </html>
 
